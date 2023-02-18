aaeda1ea8bf185f07b4f249e94823e95641177c9
y-u-y-a <47471299+y-u-y-a@users.noreply.github.com>
2023-02-18 15:13:18 +0900

    feat: output console every env

---
+++ src/const/index.ts
@@ -0,0 +1,6 @@
+export const DEV_ENV = {
+  LOCAL: 'local',
+  DEVELOP: 'develop',
+  STAGING: 'staging',
+  PROD: 'prod',
+} as const
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,21 @@ import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 
 import { AppModules } from './app.module'
+import { DEV_ENV } from './const'
 
 async function closedApiServer() {
   const server = await NestFactory.create(AppModules)
   server.useGlobalPipes(new ValidationPipe())
   await server.listen(process.env.CLOSED_API_SERVER_PORT)
-  console.log(`Listen on https://localhost:${process.env.CLOSED_API_SERVER_PORT} ...`)
+
+  switch (process.env.DEV_ENV) {
+    case DEV_ENV.LOCAL:
+      return console.log(`Connected！\n Test call to http://localhost:${process.env.CLOSED_API_SERVER_PORT}/test`)
+    case DEV_ENV.DEVELOP:
+      return console.log(`Connected！\n Test call to https://localhost:${process.env.CLOSED_API_SERVER_PORT}/test`)
+    default:
+      throw new Error('サーバー起動に失敗しました')
+  }
 }
 
 closedApiServer()
