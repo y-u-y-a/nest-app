4f6fbbf6361c08b2f64bac8e1a8a49a067f89c7c
y-u-y-a <47471299+y-u-y-a@users.noreply.github.com>
2023-02-18 14:59:08 +0900

    feat: add test module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 
+import { TestModules } from './general/test.module'
 import { ReportsModules } from './reports/reports.module'
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, envFilePath: ['.env'] }), ReportsModules],
+  imports: [ConfigModule.forRoot({ isGlobal: true, envFilePath: ['.env'] }), TestModules, ReportsModules],
 })
 export class AppModules {}
